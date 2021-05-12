55295c2c7f8bf94b2f9dc850e7092d2555d44d98
lucasventurar <lucas.ventura.r@gmail.com>
2021-05-12 18:32:18 +0200

    Redo last commit

--- src/checkpoints.py
+++ src/checkpoints.py
@@ -65,8 +65,7 @@ class CheckpointIO(object):
             scalars = self.parse_state_dict(state_dict)
             return scalars
         else:
-            import errno
-            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), filename)
+            raise FileExistsError
 
     def load_url(self, url):
         '''Load a module dictionary from url.
